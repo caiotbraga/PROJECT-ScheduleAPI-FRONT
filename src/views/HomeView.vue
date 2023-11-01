9c6a5763a86b0f766a7e928f1222361baebf3790
Caio Braga <caiotabraga@hotmail.com>
2023-11-01 09:58:22 -0300

    Created project and imported bootstrap

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,5 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <h1>Welcome to vue application</h1>
   </main>
 </template>
