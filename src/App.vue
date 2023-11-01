540eb799a55667c5a2623dc36048ab0c028a1b56
Caio Braga <caiotabraga@hotmail.com>
2023-11-01 10:45:09 -0300

    NavBar implemented

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,30 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-
     <div class="wrapper">
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+          <RouterLink class="navbar-brand" to="/">BlueSchedule</RouterLink>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <RouterLink class="nav-link active" to="/">Home</RouterLink>
+              </li>
+              <li class="nav-item">
+                <RouterLink class="nav-link" to="/about">About</RouterLink>
+              </li>
+              <li class="nav-item">
+                <RouterLink class="nav-link" to="/users">Users</RouterLink>
+              </li>
+            </ul>
+            
+          </div>
+        </div>
       </nav>
     </div>
   </header>
---
+++ src/views/User/View.vue
@@ -0,0 +1,5 @@
+<template>
+    <main>
+        <h1>This is User page</h1>
+    </main>
+</template>
