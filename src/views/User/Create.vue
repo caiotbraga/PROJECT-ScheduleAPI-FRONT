073e4d7599f27e537963c9ec98cbc563431995b6
Caio Braga <caiotabraga@hotmail.com>
2023-11-02 16:18:03 -0300

    Required informations inplemented

--- src/views/User/Create.vue
+++ src/views/User/Create.vue
@@ -5,17 +5,25 @@
                 <h4>Add User</h4>
             </div>
             <div class="card-body">
+                <!-- <ul class="alert alert-warning" v-if="errorList.length > 0">
+                    <li class="mb-0 ms-3" v-for="(error, index) in errorList" :key="index">
+                        {{ error.message }}
+                    </li>
+                </ul> -->
                 <div class="mb-3">
                     <label for="">Name</label>
                     <input type="text" v-model="model.user.userName" class="form-control" />
+                    <span v-if="buttonPressed && !model.user.userName" class="text-danger">Name is required</span>
                 </div>
                 <div class="mb-3">
                     <label for="">Email</label>
                     <input type="text" v-model="model.user.Email" class="form-control" />
+                    <span v-if="buttonPressed && !model.user.email" class="text-danger">Email is required</span>
                 </div>
                 <div class="mb-3">
                     <label for="">Number</label>
                     <input type="text" v-model="model.user.phoneNumber" class="form-control" />
+                    <span v-if="buttonPressed && !model.user.phoneNumber" class="text-danger">Phone Number is required</span>
                 </div>
                 <div class="mb-3">
                     <button type="button" @click="saveUser" class="btn btn-primary">Save</button>
@@ -30,37 +38,48 @@ import axios from 'axios';
 
 export default {
     name: 'userCreate',
-    data(){
+    data() {
         return {
+            errorList: [],
             model: {
                 user: {
                     userName: '',
                     email: '',
                     phoneNumber: ''
                 }
-                
-            }
+            },
+            buttonPressed: false
         }
     },
     methods: {
 
-        saveUser(){
+        saveUser() {
+            this.buttonPressed = true;
+
+            if (!this.model.user.userName || !this.model.user.email || !this.model.user.phoneNumber) {
+                this.errorList = [{ message: 'All fields are required' }];
+                return;
+            }
 
             axios.post('http://localhost:7156/user', this.model.user)
-            .then(res => {
-                console.log(res.data)
-                alert(res.data.message)
+                .then(res => {
 
-                this.mode.user = {
-                    userName: '',
-                    email: '',
-                    phoneNumber: ''
-                }
-            })
-            .catch(funcion (error) ,{
-                
-            });
-        } 
+                    console.log(res.data)
+                    alert("User " + this.model.user.userName + " Created")
+                    alert(res.data.message);
+
+                    this.model.user = {
+                        userName: '',
+                        email: '',
+                        phoneNumber: ''
+                    }
+                })
+                .catch(error => {
+                    this.errorList = [{ message: 'An error occurred while creating the user' }];
+                    console.error(error);
+                });
+
+        }
     },
 }
 </script>
