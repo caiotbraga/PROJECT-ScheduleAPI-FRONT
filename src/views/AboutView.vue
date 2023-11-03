5eb886c26b57ff186b5bb5ba54e4e63e3697b3c2
Caio Braga <caiotabraga@hotmail.com>
2023-11-02 23:38:56 -0300

    Update

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,9 +1,32 @@
 <template>
   <main>
-    <h1>About Page</h1>
+    <div class="container" style="margin-top: 20px;">
+      <h2>Muito prazer, <span>sou Caio Braga</span></h2>
+
+      <p>Sou estudante de Ciência da Computação em progresso na Universidade Católica de Pernambuco.
+        Sempre fui apaixonado por tecnologia e sou bastante empenhado na minha carreira e meus
+        objetivos. Sou extremamente comunicativo (até demais kk) e amo viajar, sou apaixonado pelas
+        diferentes culturas que existem ao redor do mundo e as pequenas peculiaridades que existem
+        em
+        cada lugar. Amo jogar, sempre joguei desde pequeno e gosto muito de escutar musica, sempre
+        to
+        escutando alguma música diferente.
+      </p>
+
+      <p>Tive experiência em Spring boot junto com api rest em um freelance que atuo ainda hoje e
+        também faço o desenvolvimento de paginas web para a minha universidade utilizando o
+        framework do Liferay, o liferay DXP 7.2, onde desenvolvi habilidades em html, css, json e
+        javascript.
+        Além disso já desenvolvi um sistema que faz o gerenciamento de
+        funcionários que são atrelados a departamentos(que podem ser manipulados, criados ou
+        deletados) que possuem vendas associadas a cada um e também uma aba de pesquisa onde pode
+        ser feita a requisição de vendas em um determinado periodo de tempo. Além disso sou
+        extremamente empolgado no que faço e amo aprender coisas novas assim como transmitir os
+        conhecimentos que eu adquiro durante minha carreira. Tive também algumas experiencias
+        fazendo a formatação de computadores e instalação de softwares também na minha faculdade.
+      </p>
+    </div>
   </main>
 </template>
 
-<style>
-
-</style>
+<style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,7 @@
 <template>
   <main>
-    <h1>Welcome to vue application</h1>
+    <div class="container">
+      <h1 style="margin-top: 20px;">Welcome to my Schedule app with Vue.js and .net core 6</h1>
+    </div>
   </main>
 </template>
